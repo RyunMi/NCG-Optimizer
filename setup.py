43eaab88b4613e60090148a1cb30f4d2da507f32
RyunMi <ryunxiaomi@gmail.com>
2023-01-22 01:35:26 +0800

    setup

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ import re
 
 install_requires = [
     'torch>=1.5.0',
-    'pytorch_ranger>=0.1.1',
 ]
 
 def _read_version():
     regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
     init_py = os.path.join(
-        os.path.dirname(__file__), 'torch_optimizer', '__init__.py'
+        os.path.dirname(__file__), 'ncg_optimizer', '__init__.py'
     )
     with open(init_py) as f:
         for line in f:
@@ -19,7 +18,7 @@ def _read_version():
             if match is not None:
                 return match.group(1)
         raise RuntimeError(
-            'Cannot find version in torch_optimizer/__init__.py'
+            'Cannot find version in ncg_optimizer/__init__.py'
         )
 
 
@@ -30,18 +29,20 @@ setup(
     url='https://github.com/RyunMi/NCG-optimizer',
     author='Kerun Mi @ XTU',
     author_email='ryunxiaomi@gmail.com',
-    license='MIT License',
+    license='Apache 2',
     packages=find_packages(exclude=('tests',)),
     install_requires=install_requires,
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
+        'License :: OSI Approved :: Apache Software License',  
         'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
