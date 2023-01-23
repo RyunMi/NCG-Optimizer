43e2901aec570d7af12d2502c0d34d40a6f1ff14
RyunMi <ryunxiaomi@gmail.com>
2023-01-24 00:12:17 +0800

    lcg

--- ncg_optimizer/lcg.py
+++ ncg_optimizer/lcg.py
@@ -6,6 +6,23 @@ from typing import List, Optional
 __all__ = ('LCG',)
 
 class LCG(Optimizer):
+    r"""Implements Linear Conjugate Gradient.
+
+    Arguments:
+        params: iterable of parameters to optimize or dicts defining
+            parameter groups
+        eps: term added to the denominator to improve
+            numerical stability (default: 1e-8)
+    
+    Example:
+        >>> import ncg_optimizer as optim
+        >>> optimizer = optim.LCG(model.parameters(), lr=0.1)
+        >>> optimizer.zero_grad()
+        >>> loss_fn(model(input), target).backward()
+        >>> optimizer.step()
+
+    """
+
     def __init__(
         self,
         params,
@@ -16,6 +33,48 @@ class LCG(Optimizer):
         defaults = dict(
             eps=eps,
         )
+
+        if isinstance(params, torch.Tensor):
+            raise TypeError("params argument given to the optimizer should be "
+                            "an iterable of Tensors or dicts, but got " +
+                            torch.typename(params))
         super(LCG, self).__init__(params, defaults)
 
-    def step(self,):
+
+    def step(self, closure=None):
+        r"""Performs a single optimization step (parameter update).
+
+        Arguments:
+            closure (callable): A closure that reevaluates the model and
+                returns the loss. Optional for most optimizers.
+
+        .. note::
+            Unless otherwise specified, this function should not modify the
+            ``.grad`` field of the parameters.
+        """
+
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+        
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad
+        
+        return loss
+
+
+def lcg(params,grad):
+    return 
+   
+def _linear_equation(func,params):
+    f_grads = torch.autograd.grad(func, params, create_graph=True)
+    return f_grads
+
+def _get_A(f_grad,params):
+    f_grads2 = torch.autograd.grad(f_grad, params)
+    return f_grads2
+
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 import re
 
 install_requires = [
-    'torch>=1.5.0',
+    'torch>=1.7.1',
 ]
 
 def _read(f):
@@ -58,7 +58,6 @@ setup(
         'Operating System :: Microsoft :: Windows',
         'Operating System :: OS Independent',     
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
