fcce1c71c6959226c8283a04b7842028bf448133
RyunMi <ryunxiaomi@gmail.com>
2023-01-25 22:25:12 +0800

    test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -28,7 +28,7 @@ def beale(tensor):
 
 cases = [
 #    (rosenbrock, (1.5, 1.5), (1, 1)),
-    (quadratic, (1.5, 1.5), (0, 0)),
+     (quadratic, (1.5, 1.5), (0, 0)),
 #    (beale, (1.5, 1.5), (3, 0.5)),
 ]
 
